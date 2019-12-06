d28e03033cbc492adb4497b032c64bd1e6bea80f
Lee Chow <leepuppychow@gmail.com>
2019-12-05 21:42:39 -0700

    Start of BST

---
+++ data_structures/bst.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* right = NULL;
+    Node* left = NULL;
+
+    Node(int _data) {
+        data = _data;
+    }
+};
+
+class Tree {
+public:
+    Node* root;
+    string output;
+
+    Tree(Node* _root) {
+        root = _root;
+    }
+
+    void insert(int value) {
+        _insertNode(value, root);
+        cout << "Insert " << value << " successful" << endl;
+    }
+
+    void sort() {
+        _sort(root);
+        cout << output << endl;
+    }
+
+private:
+    void _insertNode(int value, Node* current) {
+        if (value < current->data) {
+            if (!current->left) {
+                current->left = new Node(value);
+                return;
+            } else {
+                return _insertNode(value, current->left);
+            }
+        } else {
+            if (!current->right) {
+                current->right = new Node(value);
+                return;
+            } else {
+                return _insertNode(value, current->right);
+            }
+        }
+    };
+
+    void _sort(Node* current) {
+        if (current->left) {
+            _sort(current->left);
+        }
+        output += to_string(current->data) + " ";
+        if (current->right) {
+            _sort(current->right);
+        }
+    }
+};
+
+
+int main() {
+    Node root(50);
+    Tree tr(&root);
+
+    tr.insert(25);
+    tr.insert(75);
+    tr.insert(30);
+    tr.insert(20);
+
+    cout << tr.root->data << endl;
+    cout << tr.root->left->data << endl;
+    cout << tr.root->right->data << endl;
+    cout << tr.root->left->right->data << endl;
+    cout << tr.root->left->left->data << endl;
+
+    tr.sort();
+
+    return 0;
+}
+
+
+/*
+        50
+      /    \
+    25      75
+   /  \
+ 20    30
+
+*/
