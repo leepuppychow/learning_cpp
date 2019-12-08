40dc300abf6265f3c0e16b16f1233886e58c4d30
Lee Chow <leepuppychow@gmail.com>
2019-12-07 21:30:18 -0700

    Setting up classes for Graph

---
+++ data_structures/graph.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    string data;
+};
+
+class Edge {
+public:
+    Node* node1;
+    Node* node2;
+    int distance;
+};
+
+class Graph {
+public:
+
+};
+
+int main() {
+    return 0;
+};
